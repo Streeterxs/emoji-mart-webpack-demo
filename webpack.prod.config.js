38576798e60f5e5fa2ea0bf2b8c93b5698d56b4b
Streeterxs <afonso_mg3@hotmail.com>
2021-01-11 12:40:30 -0300

    break(prod-build): adding happypack broke emoji-mart css import

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const webpackCommonConfig = require('./webpack.common.config');
 const { merge } = require('webpack-merge');
+const HappyPack = require('happypack');
 
 const TerserPlugin = require("terser-webpack-plugin");
 
@@ -11,6 +12,26 @@ const outputPath = path.join(cwd, 'build');
 module.exports = merge(webpackCommonConfig, {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: 'happypack/loader?id=styles',
+        },
+      ]
+  },
+  plugins: [
+    new HappyPack({
+      id: 'js',
+      threads: 4,
+      loaders: ['babel-loader'],
+    }),
+    new HappyPack({
+      id: 'styles',
+      threads: 2,
+      loaders: ['style-loader', 'css-loader'],
+    }),
+  ],
   optimization: {
     minimizer: [new TerserPlugin({
       parallel: 4,
